b137bf74ac618167c11e52a46314ceac43a8f19c
Juan Carlos Montero <monterojcs@gmail.com>
2025-03-29 23:04:32 +0100

    feat: add filters for querying metadatas

--- src/metadata/index.ts
+++ src/metadata/index.ts
@@ -6,7 +6,7 @@
  */
 
 import { Connection } from '@salesforce/core';
-import { OrgMetadata } from '../types/sObjects';
+import { OrgMetadata, OrgMetadataTypeNames, OrgMetadataMap } from '../types/sObjects';
 import { queryApexClasses } from './apexClass';
 import { queryApexComponents } from './apexComponent';
 import { queryApexPages } from './apexPage';
@@ -26,64 +26,70 @@ import { queryCustomFields } from './customField';
 import { queryCustomObjects } from './customObject';
 import { queryStandardEntities } from './standardEntity';
 
-export async function queryMetadatas(conn: Connection): Promise<OrgMetadata> {
-  const orgMetadata = new Map();
-  const [
-    apexClasses,
-    apexComponents,
-    apexPages,
-    apexTriggers,
-    customApplications,
-    auraDefinitionBundles,
-    customLabels,
-    customTabs,
-    emailTemplates,
-    fieldSets,
-    flexiPages,
-    flows,
-    homePageComponents,
-    layouts,
-    lightningComponentBundles,
-    customFields,
-    customObjects,
-    standardEntities,
-  ] = await Promise.all([
-    queryApexClasses(conn),
-    queryApexComponents(conn),
-    queryApexPages(conn),
-    queryApexTriggers(conn),
-    queryCustomApplications(conn),
-    queryAuraDefinitionBundles(conn),
-    queryCustomLabels(conn),
-    queryCustomTabs(conn),
-    queryEmailTemplates(conn),
-    queryFieldSets(conn),
-    queryFlexiPages(conn),
-    queryFlows(conn),
-    queryHomePageComponents(conn),
-    queryLayouts(conn),
-    queryLightningComponentBundles(conn),
-    queryCustomFields(conn),
-    queryCustomObjects(conn),
-    queryStandardEntities(conn),
-  ]);
-  orgMetadata.set('ApexClass', apexClasses);
-  orgMetadata.set('ApexComponent', apexComponents);
-  orgMetadata.set('ApexPage', apexPages);
-  orgMetadata.set('ApexTrigger', apexTriggers);
-  orgMetadata.set('CustomApplication', customApplications);
-  orgMetadata.set('AuraDefinitionBundle', auraDefinitionBundles);
-  orgMetadata.set('CustomLabel', customLabels);
-  orgMetadata.set('CustomTabs', customTabs);
-  orgMetadata.set('EmailTemplate', emailTemplates);
-  orgMetadata.set('FieldSet', fieldSets);
-  orgMetadata.set('FlexiPage', flexiPages);
-  orgMetadata.set('Flow', flows);
-  orgMetadata.set('HomePageComponent', homePageComponents);
-  orgMetadata.set('Layout', layouts);
-  orgMetadata.set('LightningComponentBundle', lightningComponentBundles);
-  orgMetadata.set('CustomField', customFields);
-  orgMetadata.set('CustomObject', customObjects);
-  orgMetadata.set('StandardEntity', standardEntities);
+type QueryFunction = (conn: Connection) => Promise<OrgMetadataMap>;
+
+const queryFunctions: { [key in OrgMetadataTypeNames]: QueryFunction } = {
+  ApexClass: queryApexClasses,
+  ApexComponent: queryApexComponents,
+  ApexPage: queryApexPages,
+  ApexTrigger: queryApexTriggers,
+  CustomApplication: queryCustomApplications,
+  AuraDefinitionBundle: queryAuraDefinitionBundles,
+  CustomLabel: queryCustomLabels,
+  CustomTab: queryCustomTabs,
+  EmailTemplate: queryEmailTemplates,
+  FieldSet: queryFieldSets,
+  FlexiPage: queryFlexiPages,
+  Flow: queryFlows,
+  HomePageComponent: queryHomePageComponents,
+  Layout: queryLayouts,
+  LightningComponentBundle: queryLightningComponentBundles,
+  CustomField: queryCustomFields,
+  CustomObject: queryCustomObjects,
+  StandardEntity: queryStandardEntities,
+};
+
+export async function queryMetadatas(
+  conn: Connection,
+  options?: { include?: OrgMetadataTypeNames[]; exclude?: OrgMetadataTypeNames[] }
+): Promise<OrgMetadata> {
+  const orgMetadata = new Map<OrgMetadataTypeNames, OrgMetadataMap>();
+  const typesToQuery: OrgMetadataTypeNames[] = [];
+
+  if (options?.include) {
+    typesToQuery.push(...options.include);
+  } else {
+    for (const key in queryFunctions) {
+      if (!Object.prototype.hasOwnProperty.call(queryFunctions, key)) continue;
+      typesToQuery.push(key as OrgMetadataTypeNames);
+    }
+  }
+
+  if (options?.exclude) {
+    for (const type of options.exclude) {
+      const index = typesToQuery.indexOf(type);
+      if (index > -1) {
+        typesToQuery.splice(index, 1);
+      }
+    }
+  }
+
+  const promises: Array<Promise<OrgMetadataMap>> = [];
+  const types: OrgMetadataTypeNames[] = [];
+
+  for (const type of typesToQuery) {
+    const queryFunction = queryFunctions[type];
+    if (queryFunction) {
+      promises.push(queryFunction(conn));
+      types.push(type);
+    }
+  }
+
+  const results = await Promise.all(promises);
+
+  for (let i = 0; i < results.length; i++) {
+    orgMetadata.set(types[i], results[i]);
+  }
+
   return orgMetadata;
 }
